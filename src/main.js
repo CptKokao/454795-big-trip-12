b535a0e21cbb5f3f3ab21e537de452e8eed98431
CptKokaoHome <pro-markov@yamdex.ru>
2020-08-22 22:25:55 +0300

    Изменяет функцию render и добавляет replace

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import DayView from './view/day.js';
 import PointView from './view/point.js';
 import NoPointsView from './view/no-points.js';
 import {generatePoint} from './mock/point.js';
-import {renderPosition, render} from "./utils/render.js";
+import {renderPosition, render, replace} from "./utils/render.js";
 import {getDateTime} from "./utils/date.js";
 
 const POINT_COUNT = 20;
@@ -16,20 +16,20 @@ const mainElement = document.querySelector(`.trip-main`);
 const eventElement = document.querySelector(`.trip-events`);
 const siteListDays = eventElement.querySelector(`.trip-days`);
 
-render(mainElement, new FilterView().getElement(), renderPosition.AFTERBEGIN);
-render(mainElement, new InfoView(points).getElement(), renderPosition.AFTERBEGIN);
-render(eventElement, new SortView().getElement(), renderPosition.AFTERBEGIN);
+render(mainElement, new FilterView(), renderPosition.AFTERBEGIN);
+render(mainElement, new InfoView(points), renderPosition.AFTERBEGIN);
+render(eventElement, new SortView(), renderPosition.AFTERBEGIN);
 
 const renderPoint = (pointListElement, point) => {
   const formComponent = new FormView(point);
   const pointComponent = new PointView(point);
 
   const replaceCardToForm = () => {
-    pointListElement.replaceChild(formComponent.getElement(), pointComponent.getElement());
+    replace(formComponent, pointComponent);
   };
 
   const replaceFormToCard = () => {
-    pointListElement.replaceChild(pointComponent.getElement(), formComponent.getElement());
+    replace(pointComponent, formComponent);
   };
 
   const onEscKeyDown = (e) => {
@@ -52,16 +52,16 @@ const renderPoint = (pointListElement, point) => {
     document.addEventListener(`keydown`, onEscKeyDown);
   });
 
-  render(pointListElement, pointComponent.getElement(), renderPosition.BEFOREEND);
+  render(pointListElement, pointComponent, renderPosition.BEFOREEND);
 };
 
 const renderListEvents = (pointsList) => {
   if (pointsList.length === 0) {
-    render(eventElement, new NoPointsView().getElement(), renderPosition.BEFOREEND);
+    render(eventElement, new NoPointsView(), renderPosition.BEFOREEND);
     return;
   }
 
-  render(siteListDays, new DayView(pointsList).getElement(), renderPosition.BEFOREEND);
+  render(siteListDays, new DayView(pointsList), renderPosition.BEFOREEND);
   const days = eventElement.querySelectorAll(`.trip-days__item`);
 
   for (let i = 0; i < days.length; i++) {
--- src/utils/render.js
+++ src/utils/render.js
@@ -1,9 +1,19 @@
+import Abstract from '../view/abstract.js';
+
 export const renderPosition = {
   AFTERBEGIN: `afterbegin`,
   BEFOREEND: `beforeend`
 };
 
 export const render = (container, element, place) => {
+  if (container instanceof Abstract) {
+    container = container.getElement();
+  }
+
+  if (element instanceof Abstract) {
+    element = element.getElement();
+  }
+
   switch (place) {
     case renderPosition.AFTERBEGIN:
       container.prepend(element);
@@ -12,6 +22,25 @@ export const render = (container, element, place) => {
       container.append(element);
       break;
   }
+};
+
+export const replace = (newChild, oldChild) => {
+  if (oldChild instanceof Abstract) {
+    oldChild = oldChild.getElement();
+  }
+
+  if (newChild instanceof Abstract) {
+    newChild = newChild.getElement();
+  }
+
+  // Родительский элемент текущего узла
+  const parent = oldChild.parentElement;
+
+  if (parent === null || oldChild === null || newChild === null) {
+    throw new Error(`Can't replace unexisting elements`);
+  }
+
+  parent.replaceChild(newChild, oldChild);
 };
 
 export const createElement = (template) => {
